f95b92e71a481c7380a590267ba5c36cb61a5716
LimHoSung <wonder789@naver.com>
2017-12-31 21:35:08 +0900

    implement drawerToggle component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,20 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = ( props ) => {
-    return (
-        <Aux>
-            <Toolbar />
-            <SideDrawer />
-            <main className={classes.Content}>
-                {props.children}
-            </main>
-        </Aux>
-    )
+class Layout extends Component {
+
+    constructor ( props ) {
+        super(props);
+        this.state = {
+            showSideDrawer : false
+        }
+    }
+
+    toggleSideDrawerHandler = () => {
+        this.setState((prevState, props) => {
+            return {
+                showSideDrawer : !prevState.showSideDrawer
+            }
+        });
+    }
+
+    render () {
+        return (
+            <Aux>
+                <Toolbar
+                    drawerToggleClick={this.toggleSideDrawerHandler}
+                />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.toggleSideDrawerHandler}
+                />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
 }
 
-export default layout;
+export default Layout;
